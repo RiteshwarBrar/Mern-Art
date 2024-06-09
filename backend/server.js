6d76c1e78580281f989285b4fc00bcb317da033d
RiteshwarBrar <riteshwarbrar@gmail.com>
2024-06-09 18:29:27 -0500

    misc

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 //express application
 const app = express();
 //port
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to Nimrat's Art Store"});
+    });
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
