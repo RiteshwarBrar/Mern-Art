99fe590fedca9ae26f28be4c780fd3aa9cb82a5f
RiteshwarBrar <riteshwarbrar@gmail.com>
2024-06-09 19:06:35 -0500

    Added environment variables and updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+require('dotenv').config();
 //express application
 const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)  //log the request path and method
+    next()  //call the next middleware function
+})
+
 //port
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to Nimrat's Art Store"});
     });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+//listner for requests
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port', process.env.PORT);
 });
