882674776373436684e3455dcceceefdf4752ca9
RiteshwarBrar <riteshwarbrar@gmail.com>
2024-06-10 00:51:19 -0500

    Added Navbar. BUGS: NavBar not visible

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
+//import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home.js';
 import Paintings from './pages/paintings.js';
 import About from './pages/about.js';
 import NotFound from './pages/notfound.js';
-
+import {NavBar} from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar/>
         <div className='pages'>
           <Routes>
             <Route path='/' element={<Home />} />
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,12 @@
-const Navbar = () => {  }
+import { Link } from "react-router-dom";
+
+export const NavBar = () => {
+    <header>
+        <div className="container">
+            <Link to="/">
+                <h1>Nimrat's Art Gallery</h1>
+            </Link>
+        </div>
+    </header>
+}
+
