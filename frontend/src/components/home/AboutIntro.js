89efc462242a1dcd78e2f2a666ede73107b702fd
RiteshwarBrar <riteshwarbrar@gmail.com>
2024-07-12 17:38:05 -0500

    miscellaneous

--- frontend/src/components/home/AboutIntro.js
+++ frontend/src/components/home/AboutIntro.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const AboutIntro = () => {
     return (
