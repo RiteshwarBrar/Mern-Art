37e3765698dca80af4cec63aab64d3afafad6bbb
RiteshwarBrar <riteshwarbrar@gmail.com>
2024-07-17 16:00:15 -0500

    Deployed website using Firebase & Github Actions

--- frontend/src/components/home/Hero.js
+++ frontend/src/components/home/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className="hero">
       <div className="box">
-        <h1>Welcome to Nimrat's Art Gallery</h1>
+        <h1>Welcome to Nimrat Brar's Art Gallery</h1>
         <p>From ready-for-sale paintings to custom orders.</p>
         <Link className="shop-now" to="/paintings">Explore Now</Link>
         {/* <div className="hero-image">_</div> */}
